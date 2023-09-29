3f3a46add4124f34a2fd5fdadf3f91da74233b9b
bloodstrawberry <bloodstrawberry@github.com>
2023-09-29 15:31:23 +0900

    commit message!

---
+++ handsontable.js
@@ -0,0 +1,494 @@
+{
+  "data": [
+    [
+      "",
+      "2017",
+      "2018",
+      "2019",
+      "2020",
+      "2021",
+      "2022"
+    ],
+    [
+      "Tesla",
+      10,
+      5,
+      5,
+      10,
+      14,
+      5
+    ],
+    [
+      "Nissan",
+      15,
+      2,
+      7,
+      11,
+      13,
+      4
+    ],
+    [
+      "Toyota",
+      11,
+      1,
+      10,
+      12,
+      12,
+      3
+    ],
+    [
+      "Honda",
+      5,
+      3,
+      7,
+      13,
+      11,
+      4
+    ],
+    [
+      "Mazda",
+      4,
+      7,
+      5,
+      14,
+      10,
+      4
+    ]
+  ],
+  "cellStyle": [
+    [
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      }
+    ],
+    [
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap current area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      }
+    ],
+    [
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      }
+    ],
+    [
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      }
+    ],
+    [
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap area highlight",
+        "style": {
+          "fontWeight": "bold",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      }
+    ],
+    [
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      },
+      {
+        "className": "htMiddle htCenter htNoWrap",
+        "style": {
+          "fontWeight": "",
+          "fontStyle": "",
+          "textDecoration": "",
+          "color": "",
+          "backgroundColor": ""
+        }
+      }
+    ]
+  ],
+  "comments": [],
+  "mergeCells": []
+}
