4db43101e7d8746826aa88e58dcffdd75ad0c2f1
bloodstrawberry <bloodstrawberry@github.com>
2023-11-17 23:47:21 +0900

    make comments

---
+++ comments/original_comments.js
@@ -0,0 +1,51 @@
+[
+  {
+    "userId": "bloodstrawberry",
+    "comId": "a6507c8f-fbee-4a4d-b15a-7fe9a8102a80",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>2</p>",
+    "replies": [
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "a6507c8f-fbee-4a4d-b15a-7fe9a8102a80",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p>AA</p>",
+        "comId": "73281e4a-556e-4087-84d9-ee61e7a23b77"
+      },
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "73281e4a-556e-4087-84d9-ee61e7a23b77",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p>reply</p>",
+        "parentOfRepliedCommentId": "a6507c8f-fbee-4a4d-b15a-7fe9a8102a80",
+        "comId": "8201d930-46c4-4bba-b074-cc3b4680c1c3"
+      }
+    ]
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "58a7aa82-be63-4112-8718-d43f2d2992af",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>3</p>",
+    "replies": [
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "1ea518a1-304e-4252-b598-cbcc558478fd",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p>reply2</p>",
+        "parentOfRepliedCommentId": "58a7aa82-be63-4112-8718-d43f2d2992af",
+        "comId": "a4b4a1b8-8bf6-4377-8713-782dbca248f5"
+      }
+    ]
+  }
+]
