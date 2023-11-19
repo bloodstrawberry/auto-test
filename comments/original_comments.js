25e9ac974620050e29a54c060fb9d57f46df34d7
bloodstrawberry <bloodstrawberry@github.com>
2023-11-19 18:26:20 +0900

    make comments

---
+++ comments/original_comments.js
@@ -0,0 +1,159 @@
+[
+  {
+    "userId": "bloodstrawberry",
+    "comId": "ddeb63d7-c9f5-405b-90d5-af25ce679c33",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>5</p>",
+    "replies": [
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "ddeb63d7-c9f5-405b-90d5-af25ce679c33",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p><strong>zzzz</strong></p>",
+        "comId": "659e69b3-6555-4768-9f4f-e435ab5d7078"
+      }
+    ]
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "1850ccce-8b33-4e4a-a35e-1664459ea7fe",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>8</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "7fe53fb9-f5cc-48c5-9818-8d9b370d07e7",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>9</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "b8497b31-f87e-462f-9ad0-5fe9adb91327",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>10</p>",
+    "replies": [
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "b8497b31-f87e-462f-9ad0-5fe9adb91327",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p>10 1</p>",
+        "comId": "cf2c25f9-5112-4772-bf58-4de782273b82"
+      }
+    ]
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "cf3511e6-fde4-4922-b1b1-fea86d858370",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>11</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "ced2e382-6b80-4f5c-892e-8ed35040f7e7",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>hello</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "9133bb80-4a03-46d3-9237-89fe711b935c",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>hi</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "deb2a57f-f0bd-4f66-91e2-f0a90c2b38b4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>zzz</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "7ca792d6-64e3-4d0e-8ad4-1cdc0c9270e9",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>댓글 추가</p>",
+    "replies": [
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "7ca792d6-64e3-4d0e-8ad4-1cdc0c9270e9",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p>댓글</p>",
+        "comId": "008619db-c1f7-4628-9888-999c52fc9a87"
+      }
+    ]
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "e5e7fa9f-fec5-465f-aba2-a651a1135ede",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>new 1</p>",
+    "replies": [
+      {
+        "userId": "bloodstrawberry",
+        "repliedToCommentId": "e5e7fa9f-fec5-465f-aba2-a651a1135ede",
+        "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+        "userProfile": "https://github.com/bloodstrawberry",
+        "fullName": "bloodstrawberry",
+        "text": "<p><a href=\"https://bloodstrawberry.tistory.com/\" target=\"_self\">new reply </a>&nbsp;</p>",
+        "comId": "feffcf7c-6c80-41c8-b147-56c75fd076f2"
+      }
+    ]
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "983dda92-560f-42d3-8e7e-7259f3482e8c",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>new 2</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "3d4c3c87-4b86-4fb1-afb2-cadd39cadc5e",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>new 3</p>",
+    "replies": []
+  },
+  {
+    "userId": "bloodstrawberry",
+    "comId": "b47dce8d-77ab-4541-b8cf-88d747786321",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/40860674?v=4",
+    "userProfile": "https://github.com/bloodstrawberry",
+    "fullName": "bloodstrawberry",
+    "text": "<p>new 4</p>",
+    "replies": []
+  }
+]
