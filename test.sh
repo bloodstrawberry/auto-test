75b08fc3630dfcfe8d8c7e03f55bb2358d605c65
bloodstrawberry <vvv3334@hanmail.net>
2024-01-13 23:20:04 +0900

    Auto Commit

--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Git 저장소 초기화
+git init
+
+# 변경된 모든 파일을 스테이징
 git add .
+
+# 커밋 생성
 git commit -m "Auto Commit"
-git push origin main
+
+# 원격 저장소에 연결 (원격 저장소 URL은 실제로 사용하는 것으로 대체해야 합니다)
+git remote add origin https://github.com/bloodstrawberry/auto-test.git
+
+# main 브랜치 생성 (만약 main 브랜치가 없는 경우)
+git branch -M main
+
+# 변경 사항을 원격 저장소에 푸시
+git push -u origin main
