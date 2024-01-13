95f59cb8c418d21c5b774c0e5d1f4a9343625ac9
bloodstrawberry <vvv3334@hanmail.net>
2024-01-13 23:46:16 +0900

    Auto Commit

--- test.sh
+++ test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# 디렉토리 이동
+cd "D:/github/auto-test"
+
 # Git 저장소 초기화
 git init
+
+# 원격 저장소의 main 브랜치 가져오기
+git fetch origin main
+
+# 로컬 저장소의 main 브랜치로 원격 저장소의 main 브랜치 병합
+git merge origin/main
 
 # 변경된 모든 파일을 스테이징
 git add .
