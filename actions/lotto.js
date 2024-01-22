ac919943ea37e3027f94e8610906990322dcbc70
bloodstrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2024-01-23 01:56:27 +0900

    Update lotto.js

--- actions/lotto.js
+++ actions/lotto.js
@@ -76,8 +76,8 @@ const updateLottoJson = async () => {
     const lottoJson = JSON.parse(data);
     const lastNumber = lottoJson[lottoJson.length - 1].drwNo;
 
-    const latest = await getLottoNumber(lastNumber + 1);
-
+    //const latest = await getLottoNumber(lastNumber + 1);
+    const latest = {h : "zzz"};
     lottoJson.push(latest);
 
     const updatedJson = JSON.stringify(lottoJson, null, 2);
