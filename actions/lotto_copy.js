f8c056d5aeab7d625d9d0a81cfb3a5e1202a36dc
bloodstrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2024-08-23 12:49:41 +0900

    Update lotto_copy.js

--- actions/lotto_copy.js
+++ actions/lotto_copy.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 
+const filePath1 = "actions/lotto.js";
 const filePath = "actions/test.txt";
+
+const data = fs.readFileSync(filePath1, "utf-8");
+console.log(data)
 
 // 파일에 '1234'라는 내용을 동기적으로 씁니다.
 try {
