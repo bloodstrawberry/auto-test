d085c88be6cb8e96fe24bc50028df491ab1f3bcd
bloodstrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2024-08-23 12:24:10 +0900

    Create lotto_copy.js

---
+++ actions/lotto_copy.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const filePath = "actions/test.txt";
+
+// 파일에 '1234'라는 내용을 동기적으로 씁니다.
+try {
+    fs.writeFileSync(filePath, '1234');
+    console.log('파일에 성공적으로 쓰였습니다!');
+} catch (err) {
+    console.error('파일을 쓰는 중에 오류가 발생했습니다.', err);
+}
